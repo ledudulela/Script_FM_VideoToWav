e6eb946b8ab5d2efbee9eb72aab256086bac4a2a
ledudulela <ledudulela@free.fr>
2015-11-10 21:40:33 +0100

    màj README et VideoTo... (écrase automatiquement un fichier audio existant)

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 A partir du gestionnaire de fichier, extrait le son d'une vidéo et le convertit en wav
 
 dépendances: avconv, zenity
+
+Le script est à placer de préférence dans le répertoire /usr/local/bin/
+
+Il peut être appelé depuis le gestionnaire de fichiers.
+
+Par exemple pour Caja, en créant un lien symbolique vers /usr/local/bin/videotowav.sh
+
+dans le répertoire /home/utilisateur/.config/caja/scripts
+
+et en nommant ce lien symbolique "Extraire l'audio (wav)", il apparaitra en faisant un
+ clic-droit sur une vidéo dans Caja, un sous-menu Scripts > Extraire l'audio (wav)
+
--- VideoToMp3.sh
+++ VideoToMp3.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # auteur : ledudulela
 # version: 1.1
-# màj : 2015-11-10 21:32 
-# objet: extraire en wav le son d'une vidéo 
+# màj : 2015-11-10 21:33 
+# objet: extraire en mp3 le son d'une vidéo 
 # ---------------------------------------------------
 OLD_IFS=$IFS
 IFS='
@@ -16,7 +16,7 @@ do
 		filename=$(basename $selectedFilePath)
 		fbase=$(basename "$selectedFilePath" | awk -F "." '{print $1}')		
 		fileExt='.'$(basename "$selectedFilePath" | awk -F "." '{print $NF}') # extension du nom de fichier (dernier élément=$NF)
-		cmdToExecute='avconv -y -i "'${selectedFilePath}'" -vn -f wav "'${fbase}'.wav"' 
+		cmdToExecute='avconv -y -i "'${selectedFilePath}'" -vn -f mp3 -b:a 128k "'${fbase}'.mp3"' 
 		#zenity --info --text="${cmdToExecute}"		
 		cmdProgress='zenity --progress --title="$fbase" --text="Extraction en cours..." --width=400 --height=40 --no-cancel --auto-close 2>/dev/null'
 		eval "$cmdToExecute | $cmdProgress"
